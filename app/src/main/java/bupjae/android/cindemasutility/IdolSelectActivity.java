6a9284c244855b05e2630fca967d9491d5c4ee17
BupjaeLee <bupjae@gmail.com>
2015-04-11 22:44:49 +0900

    Move filter to Dialog

--- app/src/main/java/bupjae/android/cindemasutility/IdolSelectActivity.java
+++ app/src/main/java/bupjae/android/cindemasutility/IdolSelectActivity.java
@@ -1,41 +1,57 @@
 package bupjae.android.cindemasutility;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.LoaderManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CompoundButton;
 import android.widget.CursorAdapter;
 import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
 
 import com.androidquery.AQuery;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.TimeZone;
 
 public class IdolSelectActivity extends Activity {
     @SuppressWarnings("UnusedDeclaration")
     private static final String TAG = IdolSelectActivity.class.getSimpleName();
+    private static final String ACTION_FILTER_CHANGED = "bupjae.android.cindemasutility.action.FILTER_CHANGED";
+    private static final String EXTRA_FILTER_CONFIG = "bupjae.android.cindemasutility.extra.FILTER_CONFIG";
     public static final String EXTRA_CARD_ID = "bupjae.android.cindemasutility.extra.CARD_ID";
 
     private AQuery aq;
     private CursorAdapter adapter;
-    private Menu menu;
 
     private String query;
     private String sortOrder;
-    private String filter;
+    private String filterRarity;
+    private String filterAttribute;
+    private ArrayList<Integer> filterConfig;
 
     private LoaderManager.LoaderCallbacks<Cursor> cursorCallback = new LoaderManager.LoaderCallbacks<Cursor>() {
         @Override
@@ -44,11 +60,13 @@ public class IdolSelectActivity extends Activity {
                 case 0:
                     if (query == null) query = "";
                     if (sortOrder == null) sortOrder = "";
+                    if (filterRarity == null) filterRarity = "";
+                    if (filterAttribute == null) filterAttribute = "";
                     return new CursorLoader(
                             IdolSelectActivity.this,
                             Uri.parse("content://bupjae.android.cindemasutility.card/base"),
                             new String[]{"*", "card_id AS _id"},
-                            "card_name GLOB ?" + (filter == null ? "" : " AND rarity IN (" + filter + ")"),
+                            "card_name GLOB ?" + filterRarity + filterAttribute,
                             new String[]{"*" + query + "*"},
                             sortOrder);
                 default:
@@ -83,6 +101,32 @@ public class IdolSelectActivity extends Activity {
         }
     };
 
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            filterConfig = intent.getIntegerArrayListExtra(EXTRA_FILTER_CONFIG);
+            if (filterConfig == null) filterConfig = new ArrayList<>();
+
+            if (filterConfig.contains(R.id.filter_attribute_cute))
+                filterAttribute = " AND attribute = '큐트'";
+            else if (filterConfig.contains(R.id.filter_attribute_cool))
+                filterAttribute = " AND attribute = '쿨'";
+            else if (filterConfig.contains(R.id.filter_attribute_passion))
+                filterAttribute = " AND attribute = '패션'";
+            else filterAttribute = "";
+
+            StringBuilder builder = new StringBuilder();
+            if (filterConfig.contains(R.id.filter_rarity_1)) builder.append(", 'N'");
+            if (filterConfig.contains(R.id.filter_rarity_2)) builder.append(", 'N+'");
+            if (filterConfig.contains(R.id.filter_rarity_3)) builder.append(", 'R'");
+            if (filterConfig.contains(R.id.filter_rarity_4)) builder.append(", 'R+'");
+            if (filterConfig.contains(R.id.filter_rarity_5)) builder.append(", 'SR'");
+            if (filterConfig.contains(R.id.filter_rarity_6)) builder.append(", 'SR+'");
+            filterRarity = builder.length() == 0 ? "" : String.format(" AND rarity IN (%s)", builder.substring(2));
+
+            getLoaderManager().restartLoader(0, null, cursorCallback);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,7 +171,6 @@ public class IdolSelectActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_idol_select, menu);
-        this.menu = menu;
 
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setQueryHint(getResources().getString(R.string.search_hint));
@@ -187,14 +230,9 @@ public class IdolSelectActivity extends Activity {
                 sortOrder = "rate_defense DESC";
                 getLoaderManager().restartLoader(0, null, cursorCallback);
                 return true;
-            case R.id.filter_rarity_1:
-            case R.id.filter_rarity_2:
-            case R.id.filter_rarity_3:
-            case R.id.filter_rarity_4:
-            case R.id.filter_rarity_5:
-            case R.id.filter_rarity_6:
-                item.setChecked(!item.isChecked());
-                filterChanged();
+            case R.id.filter:
+                onFilterClicked();
+                return true;
             default:
                 return super.onMenuItemSelected(featureId, item);
         }
@@ -213,29 +251,113 @@ public class IdolSelectActivity extends Activity {
         }
     }
 
-    private void filterChanged() {
-        if (menu == null) return;
-        StringBuilder builder = new StringBuilder();
-        MenuItem item;
-        if ((item = menu.findItem(R.id.filter_rarity_1)) != null && item.isChecked()) {
-            builder.append(", 'N'");
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_FILTER_CHANGED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    private void onFilterClicked() {
+        FilterDialogFragment fragment = new FilterDialogFragment();
+        if (filterConfig != null) {
+            Bundle args = new Bundle();
+            args.putIntegerArrayList(EXTRA_FILTER_CONFIG, filterConfig);
+            fragment.setArguments(args);
         }
-        if ((item = menu.findItem(R.id.filter_rarity_2)) != null && item.isChecked()) {
-            builder.append(", 'N+'");
+        fragment.show(getFragmentManager(), "dialog");
+    }
+
+    public static class FilterDialogFragment extends DialogFragment {
+        private AQuery aq;
+        private CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) return;
+                int id = buttonView.getId();
+                if (id != R.id.filter_attribute_all)
+                    aq.id(R.id.filter_attribute_all).checked(false);
+                if (id != R.id.filter_attribute_cute)
+                    aq.id(R.id.filter_attribute_cute).checked(false);
+                if (id != R.id.filter_attribute_cool)
+                    aq.id(R.id.filter_attribute_cool).checked(false);
+                if (id != R.id.filter_attribute_passion)
+                    aq.id(R.id.filter_attribute_passion).checked(false);
+            }
+        };
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            @SuppressLint("InflateParams")
+            View content = inflater.inflate(R.layout.dialog_idol_filter, null);
+            aq = new AQuery(content);
+            Class[] signature = new Class[]{CompoundButton.OnCheckedChangeListener.class};
+            aq.id(R.id.filter_attribute_all).invoke("setOnCheckedChangeListener", signature, checkedChangeListener);
+            aq.id(R.id.filter_attribute_cute).invoke("setOnCheckedChangeListener", signature, checkedChangeListener);
+            aq.id(R.id.filter_attribute_cool).invoke("setOnCheckedChangeListener", signature, checkedChangeListener);
+            aq.id(R.id.filter_attribute_passion).invoke("setOnCheckedChangeListener", signature, checkedChangeListener);
+            fromArguments();
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.filter)
+                    .setView(content)
+                    .setCancelable(true)
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(ACTION_FILTER_CHANGED);
+                            intent.putExtras(toBundle());
+                            LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(intent);
+                        }
+                    })
+                    .create();
         }
-        if ((item = menu.findItem(R.id.filter_rarity_3)) != null && item.isChecked()) {
-            builder.append(", 'R'");
+
+        private void fromArguments() {
+            aq.id(R.id.filter_attribute_all).checked(true);
+            if (getArguments() == null) return;
+            List<Integer> idList = getArguments().getIntegerArrayList(EXTRA_FILTER_CONFIG);
+            if (idList == null) return;
+            for (int id : idList) {
+                aq.id(id).checked(true);
+            }
         }
-        if ((item = menu.findItem(R.id.filter_rarity_4)) != null && item.isChecked()) {
-            builder.append(", 'R+'");
+
+        private Bundle toBundle() {
+            class Check {
+                private ArrayList<Integer> idList = new ArrayList<>();
+
+                Check check(int id) {
+                    if (aq.id(id).isChecked()) idList.add(id);
+                    return this;
+                }
+
+                Bundle last() {
+                    Bundle ret = new Bundle();
+                    ret.putIntegerArrayList(EXTRA_FILTER_CONFIG, idList);
+                    return ret;
+                }
+            }
+            return new Check()
+                    .check(R.id.filter_attribute_all)
+                    .check(R.id.filter_attribute_cute)
+                    .check(R.id.filter_attribute_cool)
+                    .check(R.id.filter_attribute_passion)
+                    .check(R.id.filter_rarity_1)
+                    .check(R.id.filter_rarity_2)
+                    .check(R.id.filter_rarity_3)
+                    .check(R.id.filter_rarity_4)
+                    .check(R.id.filter_rarity_5)
+                    .check(R.id.filter_rarity_6)
+                    .last();
         }
-        if ((item = menu.findItem(R.id.filter_rarity_5)) != null && item.isChecked()) {
-            builder.append(", 'SR'");
-        }
-        if ((item = menu.findItem(R.id.filter_rarity_6)) != null && item.isChecked()) {
-            builder.append(", 'SR+'");
-        }
-        filter = builder.length() == 0 ? null : builder.substring(2);
-        getLoaderManager().restartLoader(0, null, cursorCallback);
     }
 }
